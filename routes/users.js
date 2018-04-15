b9e75b48d8a8e67844a99501f1d58a8e9897bacd
codeWonderland <alice.easter@mymail.champlain.edu>
2018-04-14 21:18:48 -0400

    Init commit

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "twitterAdmin",
+    password: "tacos1235"
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/getTimeline', function(req, res, next) {
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log('Connected!'); // Currently getting an error on sql queries
+        con.query('use twitter_model; ' +
+            'select username' +
+            ' from Profile' +
+            ' where username = \'' + req.get('username') + '\' and' +
+            ' pass = \'' + req.get('pass') + '\';', function (err, result) {
+            if (err)
+            {
+                console.log('Error grabbing timeline: ' + err);
+                res.send('Error grabbing timeline, check your credentials and try again');
+            }
+            res.send(result);
+        })
+    });
+});
+
+module.exports = router;
