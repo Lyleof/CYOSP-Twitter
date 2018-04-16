dfe0fc3cfc6aaf508a2ee913e59e0fe346416872
codeWonderland <alice.easter@mymail.champlain.edu>
2018-04-16 18:57:51 -0400

    Updated users.js to allow for multiple connections

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "twitterAdmin",
-    password: "tacos1235"
+var pool        = mysql.createPool({
+    connectionLimit : 10000, // default = 10
+    host            : 'localhost',
+    user            : 'twitterAdmin',
+    password        : 'tacos1235',
+    database        : 'twitter_model'
 });
 
 /* GET users listing. */
@@ -14,7 +15,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/getTimeline', function(req, res, next) {
-    con.connect(function(err) {
+    pool.getConnection(function(err, con) {
         if (err) throw err;
         console.log('Connected!'); // Currently getting an error on sql queries
         con.query('call twitter_model.get_timeline(\'' + req.get('username') + '\');'
@@ -30,7 +31,7 @@ router.get('/getTimeline', function(req, res, next) {
 });
 
 router.get('/getUser', function(req, res, next) {
-    con.connect(function(err) {
+    pool.getConnection(function(err, con) {
         if (err) throw err;
         console.log('Connected!'); // Currently getting an error on sql queries
         con.query('call twitter_model.get_user(\'' + req.get('username') + '\', \'' + req.get('pass') + '\');'
@@ -46,7 +47,7 @@ router.get('/getUser', function(req, res, next) {
 });
 
 router.get('/getUserTweets', function(req, res, next) {
-    con.connect(function(err) {
+    pool.getConnection(function(err, con) {
         if (err) throw err;
         console.log('Connected!'); // Currently getting an error on sql queries
         con.query('call twitter_model.get_user_tweets(\'' + req.get('username') + '\');'
@@ -62,10 +63,26 @@ router.get('/getUserTweets', function(req, res, next) {
 });
 
 router.get('/getTweetFavs', function(req, res, next) {
-    con.connect(function(err) {
+    pool.getConnection(function(err, con) {
         if (err) throw err;
         console.log('Connected!'); // Currently getting an error on sql queries
         con.query('call twitter_model.get_tweet_favs(\'' + req.get('tweet_id') + '\');'
+            , function (err, result) {
+                if (err)
+                {
+                    console.log('Error grabbing timeline: ' + err);
+                    res.send('Error grabbing timeline, check your credentials and try again');
+                }
+                res.send(result);
+            })
+    });
+});
+
+router.get('/getUsersFollowing', function(req, res, next) {
+    pool.getConnection(function(err, con) {
+        if (err) throw err;
+        console.log('Connected!'); // Currently getting an error on sql queries
+        con.query('call twitter_model.get_users_following(\'' + req.get('username') + '\');'
             , function (err, result) {
                 if (err)
                 {
